53f3cff7a40bf9453bacfbca88e7dbdd62738596
David den Uyl <ddenuyl@bebr.nl>
2022-10-25 08:20:32 +0200

    start creating callbacks

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 Created on 2022-10-22
 @author: David den Uyl (djdenuyl@gmail.com)
 """
-from dash import Dash
+from dash import Dash, Input, Output, ctx, State, ALL
+from dash.exceptions import PreventUpdate
 from dash.html import Div, Button
 from lib.game import Game
 
@@ -12,6 +13,7 @@ class App:
         self.dash = Dash()
         self.game = Game()
         self.dash.layout = self.layout
+        self.callbacks()
 
     @property
     def layout(self):
@@ -35,13 +37,36 @@ class App:
             for tile in row:
                 buttons.append(
                     Button(
-                        id=tile.name,
+                        id={
+                            'type': 'tile',
+                            'index': tile.name
+                        },
                         className=f'tile {tile.color}',
                         children=tile.piece
                     )
                 )
 
         return buttons
+
+    def update_tiles(self, tiles: list):
+        for i, _ in enumerate(tiles):
+            # TODO: you are here
+            tiles[i]['props']['children'] = self.game.board.get_tile(i).piece
+
+        return tiles
+
+    def callbacks(self):
+        @self.dash.callback(
+            Output('chessboard', 'children'),
+            Input({'type': 'tile', 'index': ALL}, 'n_clicks'),
+            State('chessboard', 'children'),
+            prevent_initial_callback=True
+        )
+        def render(tiles):
+            if ctx.triggered_id is None:
+                raise PreventUpdate
+
+            return self.update_tiles(tiles)
 
 
 if __name__ == '__main__':
