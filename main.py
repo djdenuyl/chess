fc7307a0372e0b17f36abc8df51d50924eb0c659
David den Uyl <ddenuyl@bebr.nl>
2023-03-19 09:13:22 +0100

    added media queries for mobile display

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """
-Created on %(date)s
+Main entrypoint for gunicorn
 
-@author: David den Uyl (ddenuyl@bebr.nl)
+author: David den Uyl (djdenuyl@gmail.nl)
+date: 2023-03-17
 """
 from app import App
 from os import environ
@@ -11,4 +12,4 @@ app = App().dash.server
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=int(environ.get("PORT", 8080)))
+    app.run(debug=True, host='0.0.0.0', port=int(environ.get('PORT', 8080)))
