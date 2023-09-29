5b11fc508844c0e923ae4e6a701b7f545ba0de5e
David den Uyl <ddenuyl@bebr.nl>
2023-09-29 17:18:33 +0200

    switching to dash-svg package instead of css pieces

--- ui/pieces/pawn.py
+++ ui/pieces/pawn.py
@@ -5,6 +5,9 @@ author: David den Uyl (djdenuyl@gmail.com)
 date: 2023-03-23
 """
 from dash.html import Div
+from dash_svg import Svg
+from parsers.svg import SVGParser
+from pathlib import Path
 from ui.pieces.piece import UIPiece
 from src.piece import PieceOption
 
@@ -14,19 +17,13 @@ class UIPawn(UIPiece):
     def piece(self) -> PieceOption:
         return PieceOption.PAWN
 
-    def build_black_piece(self) -> Div:
-        return self.builder \
-            .add_pedestal() \
-            .add_foot() \
-            .add_body() \
-            .add_body_fill() \
-            .add_shoulder() \
-            .add_shoulder_hole() \
-            .add_shoulder_fill() \
-            .add_head() \
-            .add_head_hole() \
-            .add_head_fill() \
-            .build()
+    def build_black_piece(self) -> Svg:
+        return SVGParser.from_file(
+            file=Path('assets', 'pieces', 'black', 'pawn.svg'),
+            fill='white',
+            stroke='black',
+            stroke_width=0
+        ).parse_svg()
 
     def build_white_piece(self) -> Div:
         return self.builder \
--- utils/settings.py
+++ utils/settings.py
@@ -4,4 +4,4 @@ General file settings
 author: David den Uyl (djdenuyl@gmail.com)
 date: 2023-03-25
 """
-PIECE_SCALE_FACTOR = 0.3
+PIECE_SCALE_FACTOR = 1
