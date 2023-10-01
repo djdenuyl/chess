b1923d5fc90bff22b9cdfc7019a956ad8a3c37f7
David den Uyl <ddenuyl@bebr.nl>
2023-10-01 10:18:54 +0200

    removed unnecessary revdict and settings

--- utils/dict.py
+++
@@ -1,4 +0,0 @@
-class ReversibleDict(dict):
-    @property
-    def reverse(self):
-        return {v: k for k, v in self.items()}
--- utils/settings.py
+++
@@ -1,7 +0,0 @@
-"""
-General file settings
-
-author: David den Uyl (djdenuyl@gmail.com)
-date: 2023-03-25
-"""
-PIECE_SCALE_FACTOR = 1
